cca27358b7994264fb25ea9bc08d55e702dc3a4c
making <makingx@gmail.com>
2016-09-07 22:05:44 +0900

    Add @JsonProperty

--- src/main/java/com/example/Person.java
+++ src/main/java/com/example/Person.java
@@ -1,12 +1,14 @@
 package com.example;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.io.Serializable;
 
 public class Person implements Serializable {
 	private final String name;
 	private final int age;
 
-	public Person(String name, int age) {
+	public Person(@JsonProperty("name") String name, @JsonProperty("age") int age) {
 		this.name = name;
 		this.age = age;
 	}
